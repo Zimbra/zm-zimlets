eaf2d663b568f51553c9e6a80868c9fa22a68cf1
Unknown Perforce User <jylee>
2006-08-11 18:31:00 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=9938 bug 9938 moved the last four Zimlets email, url, date, phone, out of ZimbraServer project and into Zimlet project.  added the latest zip bundle of those zimlets in the server project for dev builds.  nuked URLHandler class and use the generic RegexHandler for url zimlet.  added .project and .classpath for Zimlet project to p4.
    Copied from Perforce
     Change: 30056

---
+++ src/zimlet/com_zimbra_email/email.js
@@ -0,0 +1,209 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: ZPL 1.1
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Web Client
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+function Com_Zimbra_Email() {
+}
+
+Com_Zimbra_Email.prototype = new ZmZimletBase();
+Com_Zimbra_Email.prototype.constructor = Com_Zimbra_Email;
+
+Com_Zimbra_Email.prototype.init =
+function() {
+	this._contacts = this._appCtxt.getApp(ZmZimbraMail.CONTACTS_APP).getContactList();
+};
+
+Com_Zimbra_Email.prototype._getHtmlContent =
+function(html, idx, obj) {
+	var content = null;
+	if (obj instanceof ZmEmailAddress) {
+		if (this._appCtxt.get(ZmSetting.CONTACTS_ENABLED)) {
+			var contact = this._contacts.getContactByEmail(obj.address);
+			if (contact) {
+				content = contact.getFullName();
+			}
+		}
+		if (!content) {
+			content = obj.toString();
+		}
+	} else {
+		content = obj;
+	}
+	html[idx++] = AjxStringUtil.htmlEncode(content);
+	return idx;
+};
+
+Com_Zimbra_Email.prototype.toolTipPoppedUp =
+function(spanElement, contentObjText, matchContext, canvas) {
+	var toolTip;
+	var addr = (contentObjText instanceof ZmEmailAddress) ? contentObjText.address : contentObjText;
+	var contact;
+	if (this._appCtxt.get(ZmSetting.CONTACTS_ENABLED) && (contact = this._contacts.getContactByEmail(addr))) {
+		toolTip = contact.getToolTip(addr);
+	} else {
+		toolTip = "<b>Email: </b>" + AjxStringUtil.htmlEncode(contentObjText.toString());
+	}
+	canvas.innerHTML = toolTip;
+};
+
+Com_Zimbra_Email.prototype.getActionMenu =
+function(obj, span, context) {
+
+	// call base class first to get the action menu
+	var actionMenu = ZmZimletBase.prototype.getActionMenu.call(this, obj, span, context);
+
+	if (!this._appCtxt.get(ZmSetting.CONTACTS_ENABLED)) {
+		// make sure to remove adding new contact menu item if contacts are disabled
+		if (actionMenu.getOp("NEWCONTACT"))
+			ZmOperation.removeOperation(actionMenu, "NEWCONTACT", actionMenu._menuItems);
+	} else {
+		// bug fix #5262 - Change action menu item for contact depending on whether
+		// email address is found in address book or not.
+		if (this._contacts) {
+			var addr = (obj instanceof ZmEmailAddress) ? obj.getAddress() : obj;
+			var found = (this._contacts.getContactByEmail(addr) != null);
+			var newOp = found ? ZmOperation.EDIT_CONTACT : ZmOperation.NEW_CONTACT;
+			var newText = found ? null : ZmMsg.AB_ADD_CONTACT;
+			ZmOperation.setOperation(actionMenu, "NEWCONTACT", newOp, newText);
+		}
+	}
+	if (actionMenu.getOp("SEARCH") && !this._appCtxt.get(ZmSetting.SEARCH_ENABLED)) {
+		ZmOperation.removeOperation(actionMenu, "SEARCH", actionMenu._menuItems);
+	}
+	if (actionMenu.getOp("SEARCHBUILDER") && !this._appCtxt.get(ZmSetting.BROWSE_ENABLED)) {
+		ZmOperation.removeOperation(actionMenu, "SEARCHBUILDER", actionMenu._menuItems);
+	}
+	if (actionMenu.getOp("NEWFILTER") && !this._appCtxt.get(ZmSetting.FILTERS_ENABLED)) {
+		ZmOperation.removeOperation(actionMenu, "NEWFILTER", actionMenu._menuItems);
+	}
+
+	return actionMenu;
+};
+
+Com_Zimbra_Email.prototype.clicked =
+function(spanElement, contentObjText, matchContext, ev) {
+	var inNewWindow = (!this._appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && ev.shiftKey) ||
+					  (this._appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && !ev.shiftKey);
+	var cc = this._appCtxt.getApp(ZmZimbraMail.MAIL_APP).getComposeController();
+	cc.doAction(ZmOperation.NEW_MESSAGE, inNewWindow, null, contentObjText + ZmEmailAddress.SEPARATOR);
+};
+
+Com_Zimbra_Email.prototype.menuItemSelected =
+function(itemId, item, ev) {
+	switch (itemId) {
+		case "SEARCH":
+			this._searchListener();
+			break;
+		case "SEARCHBUILDER":
+			this._browseListener();
+			break;
+		case "NEWEMAIL":
+			this._composeListener(ev);
+			break;
+		case "NEWCONTACT":
+			this._contactListener();
+			break;
+		case "NEWFILTER":
+			this._filterListener();
+			break;
+		case "GOTOURL":
+			this._goToUrlListener();
+			break;
+	}
+};
+
+Com_Zimbra_Email.prototype._getAddress =
+function(obj) {
+	if (obj.constructor == ZmEmailAddress) {
+		return obj.address;
+	} else {
+		return obj;
+	}
+};
+
+Com_Zimbra_Email.prototype._contactListener =
+function() {
+	var contact;
+	var capp = this._appCtxt.getApp(ZmZimbraMail.CONTACTS_APP);
+
+	// actionObject can be a ZmContact, a String, or a generic Object (phew!)
+	if (this._actionObject) {
+		if (this._actionObject instanceof ZmContact)
+			contact = this._actionObject;
+		else if (AjxUtil.isString(this._actionObject))
+			contact = capp.getContactList().getContactByEmail(this._actionObject)
+		else
+			contact = capp.getContactList().getContactByEmail(this._actionObject.address);
+	}
+
+	if (contact == null) {
+		contact = new ZmContact(this._appCtxt);
+		contact.initFromEmail(this._actionObject);
+	}
+
+	capp.getContactController().show(contact);
+};
+
+Com_Zimbra_Email.prototype._composeListener =
+function(ev) {
+	var inNewWindow = (!this._appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && ev.shiftKey) ||
+					  (this._appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && !ev.shiftKey);
+	var cc = this._appCtxt.getApp(ZmZimbraMail.MAIL_APP).getComposeController();
+	cc.doAction(ZmOperation.NEW_MESSAGE, inNewWindow, null, this._getAddress(this._actionObject) + ZmEmailAddress.SEPARATOR);
+};
+
+Com_Zimbra_Email.prototype._browseListener =
+function() {
+	this._appCtxt.getSearchController().fromBrowse(this._getAddress(this._actionObject));
+};
+
+Com_Zimbra_Email.prototype._searchListener =
+function() {
+	this._appCtxt.getSearchController().fromSearch(this._getAddress(this._actionObject));
+};
+
+Com_Zimbra_Email.prototype._filterListener =
+function() {
+	var rule = new ZmFilterRule();
+	rule.addCondition(new ZmCondition(ZmFilterRule.C_FROM, ZmFilterRule.OP_IS, this._getAddress(this._actionObject)));
+	rule.addAction(new ZmAction(ZmFilterRule.A_KEEP));
+	var dialog = this._appCtxt.getFilterRuleDialog();
+	dialog.popup(rule);
+};
+
+Com_Zimbra_Email.prototype._goToUrlListener =
+function() {
+	var parts = (this._getAddress(this._actionObject)).split("@");
+	if (parts.length) {
+		var domain = parts[parts.length - 1];
+		var pieces = domain.split(".");
+		var url = (pieces.length <= 2) ? 'www.' + domain : domain;
+		this._actionUrl = "http://" + url;
+	}
+	if (this._actionUrl) {
+		window.open(this._actionUrl, "_blank", "menubar=yes,resizable=yes,scrollbars=yes");
+	} else {
+		this.displayStatusMessage("Unable to create URL from email.");
+	}
+};
---
+++ src/zimlet/com_zimbra_phone/phone.js
@@ -0,0 +1,143 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: ZPL 1.1
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Web Client
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+function Com_Zimbra_Phone() {
+}
+
+Com_Zimbra_Phone.prototype = new ZmZimletBase();
+Com_Zimbra_Phone.prototype.constructor = Com_Zimbra_Phone;
+
+Com_Zimbra_Phone.prototype.match =
+function(line, startIndex) {
+	var a = this.regexps;
+	var ret = null;
+	for (var i = 0; i < a.length; ++i) {
+		var re = a[i];
+		re.lastIndex = startIndex;
+		var m = re.exec(line);
+		if (m && (ret == null || m.index < ret.index)) {
+			if (re.useParen) {
+				for (var j = 1; j < re.useParen; ++j)
+					m.index += m[j].length;
+				m[0] = m[re.useParen];
+			}
+			ret = m;
+		}
+	}
+	return ret;
+};
+
+Com_Zimbra_Phone.prototype.init = function() {
+	var regexps = [];
+	var o = this.xmlObj().contentObject.matchOn[0];
+	var a = o.regex;
+	for (var i = 0; i < a.length; ++i) {
+		o = a[i];
+		var attrs = o.attrs;
+		if (!attrs)
+			attrs = "ig";
+		var re = new RegExp(o._content, attrs);
+		if (o.paren != null)
+			re.useParen = parseInt(o.paren);
+		regexps.push(re);
+	}
+	this.regexps = regexps;
+};
+
+Com_Zimbra_Phone.prototype._getHtmlContent =
+function(html, idx, phone, context) {
+	var call = Com_Zimbra_Phone.getCallToLink(phone);
+	html[idx++] = '<a href="' + call + '" onclick="window.top.Com_Zimbra_Phone.unsetOnbeforeunload()">' + AjxStringUtil.htmlEncode(phone) + '</a>';
+	return idx;
+};
+
+Com_Zimbra_Phone.prototype.toolTipPoppedUp =
+function(spanElement, contentObjText, matchContext, canvas) {
+	var html = [];
+	var i = 0;
+	html[i++] = "<table cellpadding=2 cellspacing=0 border=0><tr valign='center'>";
+	html[i++] = "<td>";
+	html[i++] = AjxImg.getImageHtml("Telephone");
+	html[i++] = "</td>";
+	html[i++] = "<td><b><div style='white-space:nowrap'>" + "Phone Number:" + "</div></b></td>";
+	html[i++] = "<td><div style='white-space:nowrap'>" + AjxStringUtil.htmlEncode(contentObjText) + "</div></td></tr></table>";
+	canvas.innerHTML =  html.join("");
+};
+
+Com_Zimbra_Phone.prototype.menuItemSelected = function(itemId) {
+	switch (itemId) {
+		case "SEARCH":
+			this._searchListener();
+			break;
+		case "ADDCONTACT":
+			this._contactListener();
+			break;
+		case "CALL":
+			this._callListener();
+			break;
+	}
+};
+
+Com_Zimbra_Phone.prototype._searchListener =
+function() {
+	this._appCtxt.getSearchController().search({query: this._actionObject});
+};
+
+Com_Zimbra_Phone.prototype._contactListener =
+function() {
+	var contact = new ZmContact(this._appCtxt);
+	contact.initFromPhone(this._actionObject);
+	this._appCtxt.getApp(ZmZimbraMail.CONTACTS_APP).getContactController().show(contact);
+};
+
+Com_Zimbra_Phone.prototype._callListener =
+function() {
+	var phone = Com_Zimbra_Phone.getCallToLink(this._actionObject.toString());
+	Com_Zimbra_Phone.unsetOnbeforeunload();
+	window.location = phone;
+};
+
+Com_Zimbra_Phone.resetOnbeforeunload =
+function() {
+	window.onbeforeunload = ZmZimbraMail._confirmExitMethod;
+};
+
+Com_Zimbra_Phone.unsetOnbeforeunload =
+function() {
+	window.onbeforeunload = null;
+	this._timerObj = new AjxTimedAction(null, Com_Zimbra_Phone.resetOnbeforeunload);
+	AjxTimedAction.scheduleAction(this._timerObj, 3000);
+};
+
+Com_Zimbra_Phone.getCallToLink =
+function(phoneIn) {
+	if (!phoneIn) {
+		return "";
+	}
+	var phone = AjxStringUtil.trim(phoneIn, true);
+	if (!/^(?:\+|00)/.test(phone))
+		phone = "+1" + phone;
+	return "callto:" + phone;
+};
