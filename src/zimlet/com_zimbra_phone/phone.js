066067be0b3e604e44f2a11cdfea88d895918352
Unknown Perforce User <rajar>
2011-01-28 12:56:05 -0600

    bug:53630 integrate to GNR
    http://bugzilla.zimbra.com/show_bug.cgi?id=53630
    
    Copied from Perforce
     Change: 272945

--- src/zimlet/com_zimbra_phone/phone.js
+++ src/zimlet/com_zimbra_phone/phone.js
@@ -19,6 +19,9 @@ function Com_Zimbra_Phone() {
 Com_Zimbra_Phone.prototype = new ZmZimletBase();
 Com_Zimbra_Phone.prototype.constructor = Com_Zimbra_Phone;
 
+// Consts
+Com_Zimbra_Phone.PEOPLE_SEARCH_TOOLBAR_ID	= "phone";
+
 Com_Zimbra_Phone.prototype.match =
 function(line, startIndex) {
 	var a = this.regexps;
@@ -39,36 +42,15 @@ function(line, startIndex) {
 Com_Zimbra_Phone.prototype.init =
 function() {
 	var regexps = [
-        new RegExp(this.getMessage("northAmericanNumberingPlan"),"ig"),
-        new RegExp(this.getMessage("genericInternational"), "ig")
+        new RegExp(this.getMessage("localPhoneRegEx"),"ig")
     ];
-    var localNumbers = this.getMessage("localNumbers");
-    if (localNumbers && localNumbers != "###") {
-        regexps.push(new RegExp(localNumbers, "ig"));
-    }
 	this.regexps = regexps;
+	this.countryCode = this.getMessage("countryCode");
+	if(!this.countryCode) {
+		this.countryCode = 1;
+	}
 };
-/**
-* Not needed anymore as labels are refered from zimlet specific props file now
-* Com_Zimbra_Phone.prototype.getActionMenu =
-	function(obj, span, context) {
-        var actionMenu = ZmZimletBase.prototype.getActionMenu.call(this, obj, span, context);
 
-        var op = actionMenu.getOp("SEARCH");
-        if (op) {
-            op.setText(ZmMsg.search);
-        }
-        op = actionMenu.getOp("ADDCONTACT");
-        if (op) {
-            op.setText(ZmMsg.AB_ADD_CONTACT);
-        }
-        op = actionMenu.getOp("CALL");
-        if (op) {
-            op.setText(ZmMsg.call);
-        }
-        return actionMenu;
-    };
-*/
 Com_Zimbra_Phone.prototype._getHtmlContent =
 function(html, idx, phone, context) {
 	var call = Com_Zimbra_Phone.getCallToLink(phone);
@@ -98,6 +80,30 @@ function(itemId) {
 		case "CALL":		this._callListener(); break;
 	}
 };
+
+Com_Zimbra_Phone.prototype.onPeopleSearchShow =
+function(peopleSearch, contact, rowId) {
+    peopleSearch._clearText(rowId+"-phone");
+	var phone = contact && contact.getAttr(ZmContact.F_workPhone);
+    if(phone){
+        var phoneTxt = new DwtText({parent:appCtxt.getShell(), parentElement:rowId+"-phone", index:0, id:"NewCall", className:"FakeAnchor"});
+        phoneTxt.isLinkText = true;
+        phoneTxt.setText(phone);
+        phoneTxt.addListener(DwtEvent.ONMOUSEDOWN, new AjxListener(this, this._peopleSearchItemListener));
+        phoneTxt.addListener(DwtEvent.ONMOUSEOVER, new AjxListener(this, peopleSearch.peopleItemMouseOverListener));
+        phoneTxt.addListener(DwtEvent.ONMOUSEOUT, new AjxListener(this, peopleSearch.peopleItemMouseOutListener));
+    }
+
+};
+
+Com_Zimbra_Phone.prototype._peopleSearchItemListener =
+function(ev) {
+	var workPhone = ev.target.innerHTML;
+	var phone = Com_Zimbra_Phone.getCallToLink(workPhone);
+	Com_Zimbra_Phone.unsetOnbeforeunload();
+	window.location = phone;
+};
+
 
 Com_Zimbra_Phone.prototype._searchListener =
 function() {
@@ -136,7 +142,9 @@ function(phoneIn) {
 
 	var phone = AjxStringUtil.trim(phoneIn, true);
 	if (!/^(?:\+|00)/.test(phone)) {
-		phone = "+1" + phone;
+		if(this.countryCode == 1) {//use countrycode(when its missing) only for US(for now)
+			phone = "+1" + phone;
+		}
 	}
 	return "callto:" + phone;
 };
