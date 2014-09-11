50e4340faa534ac997f06d6e2d4107154ef40dfc
Rahul Yadav <ryadav@zimbra.com>
2014-09-11 05:27:10 -0500

    [ZSS Zimlet]:
    Moving zss zimlet from ZimbraCustomerServices/telligent to zimlets folder.
    
    Copied from Perforce
     Change: 523096

---
+++ src/zimlet/com_zimbra_zss/file_explorer.js
@@ -0,0 +1,372 @@
+function com_zimbra_zss_Explorer(initObj) {
+	this.rootContainer = initObj.rootContainer;
+	this.isFolderExplorer = initObj.isFolderExplorer;
+
+	this.mezeoServerPath = initObj.vaultPath;
+	this.parentDialog = initObj.dialog;
+	
+	this.noFilesMsg = initObj.noFilesMsg;
+	this.fetchingContentMsg = initObj.fetchingContentMsg;
+	this.refreshFolderBtnText = initObj.refreshFolderBtnText;
+
+	this.selectedItems = [];
+
+	this._initUI();
+	this._addEventHandlers();
+	//Mezeo content related to the item stored here
+	this.dataKey = "mezeoContent";
+	//Prevent fetching of data again on clicking the header again
+	this.dataFetched = "dataFetched";
+	this.mezeoCache = {};
+
+	this.loadRootContainer();
+}
+
+com_zimbra_zss_Explorer.prototype._initUI = 
+function() {
+	this._createHtml();
+
+	this.refreshButton = new DwtButton({
+								parent: this.rootContainer,
+								className: "zss-refresh-button"
+							});
+	this.refreshButton.setText(this.refreshFolderBtnText);
+	this.refreshButton.setImage("Refresh");
+	this.refreshButton.reparentHtmlElement(document.getElementById(this._refreshButtonTreeCellId));
+
+	this.folderExplorer = new DwtTree({
+		parent: this.rootContainer,
+		className: "zss-folder-explorer"
+	});
+	this.folderExplorer.reparentHtmlElement(document.getElementById(this._folderTreeCellId));
+
+	this.fileExplorer = new DwtTree({
+		parent: this.rootContainer,
+		posStyle: DwtControl.RELATIVE_STYLE,
+		className: "zss-file-explorer",
+		style: (this.isFolderExplorer ? DwtTree.SINGLE_STYLE : DwtTree.CHECKEDITEM_STYLE)
+	});
+	this.fileExplorer.reparentHtmlElement(document.getElementById(this._fileTreeCellId));	
+
+	//Add a dummy file to prevent the file explorer td from collapsing
+	this._addGhostFile();
+
+	//Cache elements 
+	this._fileFolderTreeContainer = document.getElementById(this._treeWrapperTableId);
+	this._folderTreeWrapper = document.getElementById(this._folderTreeCellId);
+	this._fileTreeWrapper = document.getElementById(this._fileTreeCellId);
+
+	//Adjust the heights of the trees to prevent collapse on FF.
+	this._adjustTreeHeights();
+};
+
+com_zimbra_zss_Explorer.prototype._addEventHandlers = function(){
+	this.folderExplorer.addSelectionListener(new AjxListener(this, this._folderTreeViewListener));
+	this.fileExplorer.addSelectionListener(new AjxListener(this, this._fileTreeViewListener));
+	this.refreshButton.addSelectionListener(new AjxListener(this, this._refreshFolders));
+}
+
+com_zimbra_zss_Explorer.prototype._adjustTreeHeights = function(){
+	//Set Element Height
+	var containerDimensions = this.rootContainer.getSize();
+	var availableContentHeight = containerDimensions.y;
+	
+	// Use this height calculation if add as attachment checkbox is added again.
+	// var height = this.isFolderExplorer ? availableContentHeight - 20 : availableContentHeight - 42;
+	
+	var height = availableContentHeight - 20;
+	this._fileFolderTreeContainer.style.height = height + "px";
+	this._folderTreeWrapper.style.height = height + "px";
+	this._fileTreeWrapper.style.height = height + "px";
+}
+
+com_zimbra_zss_Explorer.prototype._adjustTreeWidths = function(){
+	var containerDimensions = this.rootContainer.getSize(),
+		availableContentWidth = containerDimensions.x;
+	this.folderExplorer.setSize(availableContentWidth * .32, this._folderTreeWrapper.style.height);
+	this.fileExplorer.setSize(availableContentWidth * .68, this._fileTreeWrapper.style.height);
+}
+
+com_zimbra_zss_Explorer.prototype._addGhostFile = function(){
+	this._addTreeItem({
+		className: "zss-ghost-file DwtTreeItem",
+		name: "",
+		path: "",
+		type: this.MEZEO_FILE,
+		icon: "",
+		content: ""
+	}, this.fileExplorer);
+}
+
+// FETCHING CONTENTS NOTIFICATION HELPERS
+com_zimbra_zss_Explorer.prototype._showFetchContentsNotification = function(){
+	$('.zss-file-explorer').append('<div class="zss-loader"><span>' + this.fetchingContentMsg + '</span></div>');
+}
+com_zimbra_zss_Explorer.prototype._hideFetchContentsNotification = function(){
+	$('.zss-file-explorer .zss-loader').remove();
+}
+//END FETCHING CONTENTS HELPER
+
+// FETCHING CONTENTS NOTIFICATION HELPERS
+com_zimbra_zss_Explorer.prototype._showNoFilesFoundMessage = function(){
+	$('.zss-file-explorer').append('<div class="zss-no-result"><span>' + this.noFilesMsg + '</span></div>');
+}
+com_zimbra_zss_Explorer.prototype._hideNoFilesFoundMessage = function(){
+	$('.zss-file-explorer .zss-no-result').remove();
+}
+//END FETCHING CONTENTS HELPER
+
+com_zimbra_zss_Explorer.prototype._createHtml = function() {
+	this._treeWrapperTableId = Dwt.getNextId();
+	this._folderTreeCellId = Dwt.getNextId();
+	this._fileTreeCellId = Dwt.getNextId();
+	this._refreshButtonTreeCellId = Dwt.getNextId();
+
+	var html = [];
+	var idx = 0;
+	var wrapperCssClass = this.isFolderExplorer? "is-chooseFolderMode" : "";	
+	
+	html[idx++] = '<table id="' + this._treeWrapperTableId + '" class="zss-tree-container ' + wrapperCssClass + '" width="100%">';
+	html[idx++] = '<tr>';
+	html[idx++] = '<td valign="top" id="' + this._refreshButtonTreeCellId + '">';
+	html[idx++] = '</td>';
+	html[idx++] = '</tr>';
+	html[idx++] = '<tr>';
+	html[idx++] = '<td valign="top" id="' + this._folderTreeCellId + '">';
+	html[idx++] = '</td>';
+	html[idx++] = '<td  valign="top"  id="' + this._fileTreeCellId + '">';
+	html[idx++] = '</td>';
+	html[idx++] = '</tr>';
+	html[idx++] = '</table>';
+
+    this.rootContainer.setContent(html.join(""));
+}
+
+com_zimbra_zss_Explorer.prototype.MEZEO_CONTAINER = "MEZEO_CONTAINER";
+com_zimbra_zss_Explorer.prototype.MEZEO_FILE = "MEZEO_FILE";
+
+com_zimbra_zss_Explorer.prototype._refreshFolders = function(){
+	this.folderExplorer.clearItems();
+	this._clearTreeItems(this.fileExplorer);
+	this.selectedItems = [];
+	this.mezeoCache = {};
+	this.loadRootContainer();
+};
+
+com_zimbra_zss_Explorer.prototype.loadRootContainer = function() {
+	this._showFetchContentsNotification();
+	this.queryServer(this.mezeoServerPath + '/v2',new AjxCallback(this, this.displayRootContainerContents));
+}
+
+com_zimbra_zss_Explorer.prototype._fileTreeViewListener  = function(ev){
+	var selectedItem = ev.item;
+	var data = selectedItem.getData(this.dataKey);
+
+	if(ev.detail === DwtTree.ITEM_CHECKED) {
+		this.onSelectItem(data, selectedItem.getChecked());
+	}
+}
+
+com_zimbra_zss_Explorer.prototype._folderTreeViewListener  = function(ev){
+	var selectedItem = ev.item;
+	var data = selectedItem.getData(this.dataKey);
+
+	if(ev.detail === DwtTree.ITEM_SELECTED ) {
+		if(this.isFolderExplorer) {
+			this.onSelectItem(data, true);
+		}
+		this.getContainerContents(data.path, selectedItem);
+	}
+	else if(ev.detail === DwtTree.ITEM_EXPANDED){
+		selectedItem.setExpanded(true,false,false);
+	}
+};
+com_zimbra_zss_Explorer.prototype.displayRootContainerContents = function(contents) {
+	this._hideFetchContentsNotification();
+	//Adjust the tree widths to accommodate results.
+	this._adjustTreeWidths();
+	
+
+	contents =  JSON.parse(contents.text);
+	if(contents) {
+		if(contents.cloud && contents.cloud.locations) {
+			var locations = contents.cloud.locations;
+			for(var i = 0, count = locations.length; i < count; i++) {
+				var location = locations[i];
+
+				var item = this._addTreeItem({
+						className: "mezeo-root DwtTreeItem",
+						name: location.name,
+						path: location.rootContainer,
+						type: this.MEZEO_CONTAINER,
+						icon: "Folder",
+						id: "mezeo-root",
+						headerItem: true,
+						content: location
+					},
+					this.folderExplorer);
+
+				this.getContainerContents(location.rootContainer,item);
+			}
+		}
+	}
+};
+
+com_zimbra_zss_Explorer.prototype.getContainerContents = function(path,parentContainer) {
+	var dataInCache = this.mezeoCache[path + "/contents"];
+	if(!this.isFolderExplorer) {
+		this.clearSelection();
+	}
+	this._showFetchContentsNotification();
+	if(!dataInCache) {
+		this.queryServer(path + "/contents", new AjxCallback(this, this._handleGetContainerContents, { parent: parentContainer, refetched: false } ));
+	}
+	else {
+		this._handleGetContainerContents( { parent: parentContainer, refetched: true }, dataInCache);
+	}
+}
+
+com_zimbra_zss_Explorer.prototype._addTreeItem = function(mezeoItem, parent) {
+	var treeItemProperties = {
+				arrowDisabled: true,
+				dynamicWidth: true,
+				className: mezeoItem.className,
+				index: parent.getChildren().length,
+				parent: parent,
+				imageInfo : mezeoItem.icon,
+				path: mezeoItem.path,
+				text: mezeoItem.name,
+			}
+
+	var item = mezeoItem.headerItem ? 
+				new DwtHeaderTreeItem(treeItemProperties)
+				: new DwtTreeItem(treeItemProperties);
+
+	item.setData(this.dataKey,{
+		type: mezeoItem.type,
+		path: mezeoItem.path,
+		content: mezeoItem.content,
+		self: item
+	});
+
+	return item;
+};
+
+com_zimbra_zss_Explorer.prototype._handleGetContainerContents = function(extraData, contents) {
+	this._hideFetchContentsNotification();
+	var parent = extraData.parent,
+		refetched = extraData.refetched;
+	this._clearTreeItems(this.fileExplorer);
+	// this._clearTreeItems(parent);
+	var mezeoContainer, filesTree;
+	var contentsJson =  JSON.parse(contents.text);
+
+	if(contentsJson["file-list"]){
+		if(contentsJson["file-list"]["file-list"]){
+			
+			this.mezeoCache[contentsJson["file-list"].uri] = contents;
+			
+			var containerContents = contentsJson["file-list"]["file-list"];
+			this._hideNoFilesFoundMessage();
+			
+			for(var i = 0, count = containerContents.length; i < count; i++){
+				var content = containerContents[i];
+				var mezeoItem = {};
+
+				if(content.container && !refetched){
+					var item = this._addTreeItem({
+						className: "mezeo-container DwtTreeItem",
+						name: content.container.name,
+						path: content.container.uri,
+						type: this.MEZEO_CONTAINER,
+						icon: "Folder",
+						content: content,
+						forceNotifySelection: true,
+						headerItem: true
+					}, parent);
+				}
+				if(content.file){
+					var item = this._addTreeItem({
+						className: "zss-file DwtTreeItem",
+						name: this._createFileHtml(content.file),
+						path: content.file.uri,
+						type: this.MEZEO_FILE,
+						icon: "GenericDoc",
+						content: content
+					}, this.fileExplorer);
+					item.enableSelection(false);
+				}
+			}
+			//Show No files found
+			if(containerContents.length === 0 || this.fileExplorer.getChildren().length === 0){
+				//Add a ghost file to prevent fileExplorer td from collapsing
+				this._addGhostFile();
+				this._showNoFilesFoundMessage();				
+			}
+
+			parent.setExpanded(true,false,false);
+		}
+	}
+}
+
+com_zimbra_zss_Explorer.prototype._clearTreeItems = function(parent){
+	while(parent.getChildren().length){
+		parent.removeChild(parent.getChildren()[0]);
+	}
+}
+
+com_zimbra_zss_Explorer.prototype._createFileHtml = function(file) {
+	var html = [],
+		i = 0;
+ 	
+ 	html[i++] = "<div class='zss-file-name'>" + file.name +"</div>";
+ 	html[i++] = "<div class='zss-file-details'>";
+ 	html[i++] = "<span class='zss-file-size'>" + AjxUtil.formatSize(file.bytes, true, null) +"</span>";
+ 	html[i++] = "</div>";
+
+ 	return html.join("");
+}
+
+com_zimbra_zss_Explorer.prototype.onSelectItem = function(file,selected){
+	if(selected) {
+		if(this.isFolderExplorer){
+			this.clearSelection();	
+		}
+		this.selectedItems.push(file);
+	}
+	else {
+		for(var i = 0, len = this.selectedItems.length; i < len; i++) {
+			if(this.selectedItems[i].path === file.path) {
+				this.selectedItems.splice(i,1);
+				return;
+			}
+		}
+	}
+}
+
+com_zimbra_zss_Explorer.prototype.getSelection = function(){
+	// var selectedFiles = this.fileExplorer.getSelection();
+	return this.selectedItems;
+}
+
+com_zimbra_zss_Explorer.prototype.clearSelection = function(){
+	this.fileExplorer.deselectAll();
+	for(var i = 0, len = this.selectedItems.length; i < len; i++) {
+		this.selectedItems[i].self.setChecked(false);
+	}
+	this.selectedItems = [];
+}
+
+
+com_zimbra_zss_Explorer.prototype.queryServer = function(extServerUrl, callback, appendHost) {
+	extServerUrl = extServerUrl.substring(extServerUrl.indexOf('/zss'));
+	var hdrs = [];
+	hdrs["X-Client-Specification"] = 3; //must pass the length of data
+	hdrs["X-Cloud-Depth"] = 1;
+	// url encode the external server url
+	// since it will part of the query params for the proxy servet
+	var encodedExtServerUrl = AjxStringUtil.urlComponentEncode(extServerUrl);
+	
+	// submit the URL and asynchronous response (using callback)
+	AjxRpc.invoke(null, encodedExtServerUrl, hdrs, callback, true);
+};
---
+++ src/zimlet/com_zimbra_zss/zss.js
@@ -0,0 +1,360 @@
+//Zimlet Class
+function com_zimbra_zss_HandlerObject() {
+	
+}
+
+//Make Zimlet class a subclass of ZmZimletBase class - this is what makes a JS class a Zimlet
+com_zimbra_zss_HandlerObject.prototype = new ZmZimletBase();
+com_zimbra_zss_HandlerObject.prototype.constructor = com_zimbra_zss_HandlerObject;
+
+
+var ZssZimlet = com_zimbra_zss_HandlerObject;
+
+ZssZimlet.prototype.init = function(){
+	this.vaultPath = this.getUserProperty("zss_url");
+	
+	this.messages = {
+		menuItemTxt: this.getMessage('menuItem'),
+		noFilesFound: this.getMessage('noFilesFound'),
+		fetchingContentMsg: this.getMessage('fetchingContent'),
+		addFilesAsAttachment: this.getMessage('addFileAsAttachment'),
+		attachingFileNotification : this.getMessage('attachingFileNotification'),
+		selectFilesDialogTitle : this.getMessage('selectFilesDialogTitle'),
+		chooseFolderDialogTitle : this.getMessage('chooseFolderDialogTitle'),
+		fileSavedToVault: this.getMessage('fileSavedToVault'),
+		saveToVaultLink: this.getMessage('saveToVaultLink'),
+		refreshFolderBtnText: this.getMessage('refreshFolderBtnText')
+	};
+	
+
+	// Add Save Attachment to Vault options in email
+	if (appCtxt.get(ZmSetting.MAIL_ENABLED)) {
+		AjxPackage.require({name:"MailCore", callback:new AjxCallback(this, this.addAttachmentHandler)});
+	}
+}
+
+/*
+	Add Zimbra Sync and Share option to attach menu
+*/
+ZssZimlet.prototype.initializeAttachPopup =
+function(menu, controller) {
+	var mi = controller._createAttachMenuItem(menu,this.messages.menuItemTxt,
+			new AjxListener(this, this.showVaultFileChooser));
+};
+
+
+
+
+/*
+	ADD VAULT FILE AS AN ATTACHMENT
+*/
+
+// code for showing Vault file chooser in a dialog and 
+// providing user the ability to select file.
+// provide ability to select whether to save vault file as an attachment or a link?
+// call -> processSelectedVaultFile
+ZssZimlet.prototype.showVaultFileChooser =
+function() {
+	if(this.dialog){
+		this.fileExplorer.clearSelection();
+		//FUNC: uncomment this if add as attachment is enabled
+		// this.addAsAttachmentCheckbox.setSelected(false);
+		this.dialog.popup();
+		return;
+	}	
+
+	this.dialogView = new DwtComposite(this.getShell());
+	this.dialogView.setSize(560, 320); //set width and height
+	this.dialogView.getHtmlElement().style.overflow = "hidden"; //adds scrollbar
+	
+	
+	this.dialog = this._createDialog({
+							title: this.messages.selectFilesDialogTitle,
+							view:this.dialogView, 	
+							standardButtons:[
+								DwtDialog.OK_BUTTON, 
+								DwtDialog.CANCEL_BUTTON
+							]
+						});
+
+	this.dialog.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._processSelectedVaultFile));
+
+	if(!this.fileExplorer){
+		this.fileExplorer = new com_zimbra_zss_Explorer({
+			rootContainer : this.dialogView,
+			vaultPath: this.vaultPath,
+			dialog: this.dialog,
+			noFilesMsg: this.messages.noFilesFound,
+			fetchingContentMsg: this.messages.fetchingContentMsg,
+			refreshFolderBtnText: this.messages.refreshFolderBtnText
+
+		});
+	}
+	
+	//FUNC: uncomment this if add as attachment is enabled
+	// this.addAsAttachmentCheckbox = new DwtCheckbox({
+	// 	parent: this.dialogView,
+	// 	style: DwtCheckbox.TEXT_RIGHT,
+	// 	name: 'zss_add_as_attachment',
+	// 	id: Dwt.getNextId()
+	// });
+	// this.addAsAttachmentCheckbox.setText(this.messages.addFilesAsAttachment);
+
+	//show the dialog
+	this.dialog.popup();
+};
+
+// Get the info of the file selected by the user 
+// addFileAsAttachment == true? (addFilesAsAttachment)
+// else add the link to the message. (addFilesAsLinkInMsg)
+ZssZimlet.prototype._processSelectedVaultFile =
+function() {
+	this.dialog.popdown();
+	var selectedFiles = this.fileExplorer.getSelection();
+	if( selectedFiles.length ) {
+		//FUNC: uncomment this if add as attachment is enabled
+		//var insertAsAttachment = this.addAsAttachmentCheckbox.isSelected();
+		
+		var insertAsAttachment = false;
+		if(insertAsAttachment) {
+			this.addFilesAsAttachment(selectedFiles);
+		}
+		else {
+			this.addFilesAsLinkInMsg(selectedFiles);
+		}
+	}
+};
+
+// add the file path as a link
+ZssZimlet.prototype.addFilesAsLinkInMsg =
+function(files) {
+	var view = appCtxt.getCurrentView();
+	var editor = view.getHtmlEditor();
+	var editorContent =  editor.getContent();
+	var isHtml = view && view.getComposeMode() === "text/html";
+	if (isHtml) {
+		var ed = editor.getEditor();
+		for(var i = 0, len = files.length; i < len; i++) {
+			var div = generateHTML(files[i]);
+			//tinymce modifies the source when using mceInsertContent
+			ed.execCommand('mceInsertRawHTML', false, div, {skip_undo : 1});
+		}
+	}
+	else {
+		for(var i = 0, len = files.length; i < len; i++) {
+			view.getHtmlEditor().setContent(editorContent + "\n" + files[i].name + " : " + files[i].path + "\n");
+		}
+	}	
+
+	function generateHTML(file){
+		var thumbnail = file.content.file.thumbnail.uri;
+		var fileName = file.content.file.name;
+		var filePath = file.content.file.content.uri;
+
+		var div = '<div style="background-color:rgb(245, 245, 245); padding:10px 14px; margin-right:10px; color:rgb(34, 34, 34);display:inline-block;margin-left:10px; '; 
+			div+='font-family:arial; font-style:normal; font-weight:bold; font-size:13px; cursor:default; border:1px solid rgb(221, 221, 221); float:left;text-align: center;">';
+			div+='<a href="' + filePath + '/inline/" target="_blank"><img style="margin-bottom:7px; border:none;" height="64" src="' + thumbnail + '?s=preview"></a>';
+			div+='<div dir="ltr" title="' + fileName + '" style="color:rgb(17, 85, 204); text-decoration:initial; vertical-align:bottom;">';
+			div+='<a href="' + filePath + '/inline/" target="_blank" style=" display:inline-block; overflow:hidden; text-overflow:ellipsis; white-space:nowrap; text-decoration:none; text-align:center; cursor:pointer;padding:1px 0; border:none; max-width:200px;">' + fileName + '</div></a>';
+			div+='</div><div style="clear:both"><br/></div>';
+
+		return div;
+	}
+};
+
+// send file details to the server to download it and create an attachmentId.
+ZssZimlet.prototype.addFilesAsAttachment =
+function(selectedFiles) {
+	if(selectedFiles.length) {
+		//Get the collection of Attachment Requests
+		var requests = this._createAttachmentRequests(selectedFiles);
+		var jsonObj = {
+			BatchRequest: {
+				_jsns: "urn:zimbra",
+				AttachMezeoFileRequest: requests
+			}
+		}
+	   var params = {
+	        jsonObj: jsonObj,
+	        asyncMode: true,
+	        callback: (new AjxCallback(this, this._addGeneratedAttachmentIdsToMsg)),
+	        errorCallback: (new AjxCallback(this, this._addGeneratedAttachmentIdsToMsgError))
+	    };
+		appCtxt.getAppController().setStatusMsg(this.messages.attachingFileNotification, ZmStatusView.LEVEL_INFO);
+		appCtxt.getAppController().sendRequest(params);
+		
+	}
+};
+
+// Create Individual Attachment requests and return the array of requests
+ZssZimlet.prototype._createAttachmentRequests = 
+function (files) {
+	var requests = [];
+
+	for (var i = files.length - 1; i >= 0; i--) {
+		var file = files[i];
+		var AttachMezeoFileRequest = {
+			"_jsns" : "urn:zimbraMail",
+			attach : { 
+				uri: file.path
+			}
+		};
+		
+		requests.push(AttachMezeoFileRequest);
+	};
+	return requests;
+}
+
+ZssZimlet.prototype._addGeneratedAttachmentIdsToMsg =
+function(response) {
+	response = response.getResponse();
+
+	//To save multiple attachments in a draft we require a comma separated string of attachment ids.
+	var attachmentIds = this._getAttachmentIdsFromResponse(response);
+	var view = appCtxt.getCurrentView();
+	var callback = new AjxCallback(this, this._handleSaveDraftCallback);
+	
+	// Need to save the msg as draft to add the attachment
+	view._controller.saveDraft(ZmComposeController.DRAFT_TYPE_MANUAL, attachmentIds, null, callback, null);
+};
+
+// Returns a string of comma separated attachment ids
+ZssZimlet.prototype._getAttachmentIdsFromResponse = 
+function (response) {
+	var attachmentIds = [];
+	var attachMezeoFileResponse = response.BatchResponse.AttachMezeoFileResponse;
+	for(var i = 0, len = attachMezeoFileResponse.length; i < len; i++) {
+		var attachResponse = attachMezeoFileResponse[i];
+		attachmentIds.push(attachResponse.attach.aid);
+	}
+	return attachmentIds.join(",");
+}
+
+ZssZimlet.prototype._handleSaveDraftCallback =
+function(response) {
+	//Enable attach button.
+	//Add error handling here.
+};
+
+
+ZssZimlet.prototype._addGeneratedAttachmentIdsToMsgError =
+function(response) {
+	// handle server errors if any while creating the attachment using mezeo path.
+	appCtxt.getAppController().setStatusMsg(response.msg, ZmStatusView.LEVEL_WARNING);
+};
+
+
+
+
+
+
+/*
+	SAVE ATTACHMENT FROM EMAIL TO VAULT
+*/
+ZssZimlet.prototype.addAttachmentHandler = function()
+{
+	this._msgController = AjxDispatcher.run("GetMsgController");
+	var viewType = appCtxt.getViewTypeFromId(ZmId.VIEW_MSG);
+	this._msgController._initializeView(viewType);
+
+	for (var mimeType in ZmMimeTable._table ) {
+		this._msgController._listView[viewType].addAttachmentLinkHandler(mimeType,"Vault",this.addVaultLink);
+	}
+	
+};
+ZssZimlet.prototype.addVaultLink = 
+function(attachment) {
+	var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_zss').handlerObject;
+	var html =
+			"<a href='#' class='AttLink' style='text-decoration:underline;' " +
+					"onClick=\"ZssZimlet.saveAttachmentToVault('" + attachment.mid + "','" + attachment.part + "','" + attachment.label+ "', '" + attachment.ct + "')\">"+
+					zimletInstance.messages.saveToVaultLink +
+					"</a>";
+	
+	return html;
+};
+
+ZssZimlet.saveAttachmentToVault =
+function(attachmentId, part, name) {
+	var attachment = {
+		id: attachmentId,
+		part: part,
+		name: name
+	}
+	var self = appCtxt._zimletMgr.getZimletByName('com_zimbra_zss').handlerObject;
+ 	
+ 	if(self.folderChooserDialog){
+		self.folderExplorer.clearSelection();
+		self.folderChooserDialog.popup();
+		return;
+	}	
+
+	self.dialogFolderView = new DwtComposite(self.getShell());
+	self.dialogFolderView.setSize(560, 320); //set width and height
+	self.dialogFolderView.getHtmlElement().style.overflow = "hidden"; //adds scrollbar
+	
+	
+	self.folderChooserDialog = self._createDialog({
+							title: self.messages.chooseFolderDialogTitle,
+							view:self.dialogFolderView, 	
+							standardButtons:[
+								DwtDialog.OK_BUTTON, 
+								DwtDialog.CANCEL_BUTTON
+							]
+						});
+
+	self.folderChooserDialog.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(self, self._useSelectedVaultContainer, attachment));
+
+	if(!self.folderExplorer){
+		self.folderExplorer = new com_zimbra_zss_Explorer({
+			rootContainer : self.dialogFolderView,
+			vaultPath: self.vaultPath,
+			dialog: self.dialog,
+			isFolderExplorer: true,
+			noFilesMsg: self.messages.noFilesFound,
+			fetchingContentMsg: self.messages.fetchingContentMsg,
+			refreshFolderBtnText: self.messages.refreshFolderBtnText
+		});
+	}
+	self.folderChooserDialog.popup();
+};
+
+ZssZimlet.prototype._useSelectedVaultContainer =
+function(attachment) {
+	this.folderChooserDialog.popdown();
+	var selectedContainer = this.folderExplorer.getSelection();
+	if(selectedContainer.length) {
+		// Send the details to the server to save the attachment to the selected vault container.
+		var jsonObj = {SaveAttachmentToMezeoRequest: {"_jsns": "urn:zimbraMail"}};
+		var request = jsonObj.SaveAttachmentToMezeoRequest;
+		
+		request.attach = {
+							mid: attachment.id,
+							part: attachment.part,
+							name: attachment.name,
+							uri: selectedContainer[0].path
+						};
+
+	    var params = {
+	        jsonObj: jsonObj,
+	        asyncMode: true,
+	        callback: (new AjxCallback(this, this._handleSaveAttachmentToVaultResponse)),
+	        errorCallback: (new AjxCallback(this, this._addGeneratedAttachmentIdsToMsgError))
+	    };
+	    appCtxt.getAppController().sendRequest(params);
+	}
+};
+
+ZssZimlet.prototype._handleSaveAttachmentToVaultResponse =
+function(response) {
+	response = response.getResponse();
+	// make this more robust
+	if(response.SaveAttachmentToMezeoResponse.attach.uri) {
+		appCtxt.getAppController().setStatusMsg(this.messages.fileSavedToVault, ZmStatusView.LEVEL_INFO);
+	}
+};
+ZssZimlet.prototype._handleSaveAttachmentToVaultError =
+function(error) {
+	//handle save to vault error
+	console.log(error);
+};
