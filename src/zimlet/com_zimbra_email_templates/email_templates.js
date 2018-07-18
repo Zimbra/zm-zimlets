ca8a2210fb6ad1a16126c832641410314f38880b
Nilesh Pawar <nilsh.pawar@synacor.com>
2018-07-18 05:48:52 -0400

    NICPS-281: Email Template - create email template zimlet part2

--- src/zimlet/com_zimbra_email_templates/email_templates.js
+++ src/zimlet/com_zimbra_email_templates/email_templates.js
@@ -59,39 +59,33 @@ function(app, toolbar, controller, viewId) {
 		this._viewIdAndMenuMap[this.viewId] = {menu:menu, controller:controller, button:button};
 		button.removeAllListeners();
 		button.removeDropDownSelectionListener();
-		button.addSelectionListener(new AjxListener(this, this._addMenuItems, [button, menu]));
-		button.addDropDownSelectionListener(new AjxListener(this, this._addMenuItems, [button, menu]));
+		var templatesData = this._getUserTemplates();
+
+		button.addSelectionListener(new AjxListener(this, this._addMenuItems, templatesData));
+		button.addDropDownSelectionListener(new AjxListener(this, this._addMenuItems , templatesData));
 	}
 };
 
-
 //--------------------------------------------------------------------------------------------------
-// GET TEMPLATE NAMES FOR USER BASED on LDAP ATTRIBUTES   (Domain + User Spcific)
+// GET TEMPLATES DATA FOR A USER BASED on LDAP ATTRIBUTES   (Domain + User Specific)
 //--------------------------------------------------------------------------------------------------
-Com_Zimbra_Email_Templates.prototype._addMenuItems =
-function(removeChildren, result) {
+Com_Zimbra_Email_Templates.prototype._getUserTemplates =
+function() {
 	var domainTemplateIds = appCtxt.getSettings().getInfoResponse.attrs._attrs.zimbraDomainTemplateID;
 	var userTemplateIds   = appCtxt.getSettings().getInfoResponse.attrs._attrs.zimbraCustomTemplateID;
 
 	var allTemplateIds = '';
-	if(typeof(domainTemplateIds) != undefined ) {
+	if(typeof(domainTemplateIds) != 'undefined' ) {
 		allTemplateIds += domainTemplateIds;
 	}
-	if(typeof(userTemplateIds) != undefined ) {
+	if(typeof(userTemplateIds) != 'undefined' ) {
 		if(allTemplateIds != '') { 
 			allTemplateIds += ',';
 		}
 		allTemplateIds += userTemplateIds;
 	}
 
-	var menu = this._viewIdAndMenuMap[this._currentViewId].menu;
-
-	// Prevents the situation of all the items being added twice to the menu
-	if (menu.getItemCount() > 0) removeChildren = true;
-	if (removeChildren) {
-		menu.removeChildren();
-	}
-
+	var templateData = '{"count":0,"result":null}';
 	if(allTemplateIds != "") {
 		var hostname = window.location.origin;
 		var jspUrl   = hostname + "/public/templatesProxy.jsp?id=" + allTemplateIds;
@@ -99,21 +93,31 @@ function(removeChildren, result) {
 
 		if (response.success == true) {
 			var jsonData = JSON.parse(response.text);
-			if (jsonData.count > 0) {
-				for (var i = 0; i < jsonData.count; i++) {
-					var templateData = jsonData.result[i];
-					var mi = menu.createMenuItem(templateData.Id, {image:"Edit", text:templateData.Template_Name});
-					mi.addSelectionListener(new AjxListener(this, this.templateClick, templateData));
-				}
-			} else {
-				menu.createMenuItem("No_Templates", {text: "No template assigned"});
+			if (jsonData.hasOwnProperty('count') && jsonData.count > 0) {
+				return jsonData;
+			}
+		}
+	}
+	return templateData;
+};
+
+//--------------------------------------------------------------------------------------------------
+// POPULATE THE EMAIL TEMPLATES DROP DOWN MENU
+//--------------------------------------------------------------------------------------------------
+Com_Zimbra_Email_Templates.prototype._addMenuItems =
+function(jsonData) {
+	var menu = this._viewIdAndMenuMap[this._currentViewId].menu;
+	if (!menu._loaded) {
+		if (jsonData.count > 0) {
+			for (var i = 0; i < jsonData.count; i++) {
+				var templateData = jsonData.result[i];
+				var mi = menu.createMenuItem(templateData.Id, {image:"Edit", text:templateData.Template_Name});
+				mi.addSelectionListener(new AjxListener(this, this.templateClick, templateData));
 			}
 		} else {
-			menu.createMenuItem("No_Templates", {text: "No template available"});
-			console.log(response);
+			menu.createMenuItem("No_Templates", {text: "No templates available"});
 		}
-	} else {
-		menu.createMenuItem("No_Templates", {text: "No template assigned"});
+		menu._loaded = true;
 	}
 
 	var button = this._viewIdAndMenuMap[this._currentViewId].button;
@@ -122,13 +126,40 @@ function(removeChildren, result) {
 };
 
 //--------------------------------------------------------------------------------------------------
-// GET TEMPLATE DATA BASED ON TEMPLATE AND UPDATE THE EDITOR CONTENT WITH TEMPLATE DATA
+// UPDATE THE EDITOR CONTENT WITH TEMPLATE DATA
 //--------------------------------------------------------------------------------------------------
 Com_Zimbra_Email_Templates.prototype.templateClick = function(params) {
-	var templateMsg = params.Template_Header + "<br/ ><br/ >";
-	templateMsg += params.Template_Body + "<br/ ><br/ >";
-	templateMsg += params.Template_Footer;
+	
+	params.Template_Header = this.replacePlaceHolders("header", params.Template_Header);
+	params.Template_Footer = this.replacePlaceHolders("footer", params.Template_Footer);
+	
+	var editor_template_main   = "style='position:relative;'";
+	var editor_template_header = "style='pointer-events: none;'";
+	var editor_template_body   = "style='text-align:justify;'";
+	var editor_template_footer = "style=''";
+
+	var templateMsg = "<div class='editor_template_main' "                  + editor_template_main   + ">";
+	templateMsg    += "<div class='editor_template_header mceNonEditable' " + editor_template_header + ">" + params.Template_Header + "<br/></div>";
+	templateMsg    += "<div class='editor_template_body' "                  + editor_template_body   + ">" + params.Template_Body   + "<br/><br/></div>";
+	templateMsg    += "<div class='editor_template_footer mceNonEditable' " + editor_template_footer + ">" + params.Template_Footer + "</div>";
+	templateMsg    += "</div>";
 	
 	var composeView = appCtxt.getCurrentView();
 	composeView.getHtmlEditor().setContent(templateMsg);
+};
+
+//--------------------------------------------------------------------------------------------------
+// REPLACE THE PLACEHOLDERS IN THE TEMPLATE DATA WITH LDAP ATTRBUTE VALUES
+//--------------------------------------------------------------------------------------------------
+Com_Zimbra_Email_Templates.prototype.replacePlaceHolders = function(contentType, contentData) {
+	if(contentType == "header") {
+		var userDesignation = appCtxt.getSettings().getInfoResponse.attrs._attrs.zimbraCustomTemplateUserDesignation;
+		contentData = contentData.replace("[[UserDesignation]]", userDesignation);
+	} else if(contentType == "footer") {
+		if(appCtxt.getSettings().getInfoResponse.signatures.signature[0] != 'undefined'){
+			var userSignature = appCtxt.getSettings().getInfoResponse.signatures.signature[0].content[0]._content
+			contentData = contentData.replace("[[UserSignature]]", userSignature);
+		}
+	}
+	return contentData;
 };
